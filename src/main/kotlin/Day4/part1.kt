6139436ce7da4d74f01daf6fe3f6903ec0495169
MrCode <reinemic2.0@gmail.com>
2022-12-07 18:34:30 +0100

    Completed Day4

---
+++ src/main/kotlin/Day4/part1.kt
@@ -0,0 +1,15 @@
+package Day4
+
+import java.io.File
+
+fun main() {
+    println(File("src/main/kotlin/Day4/input1.txt").readLines().map {
+        val line = it.split(",")
+        val range1 = line[0].split("-")
+        val range2 = line[1].split("-")
+        Pair(range1[0].toInt()..range1[1].toInt(),range2[0].toInt()..range2[1].toInt())
+    }.map {
+        val intersect = it.first.intersect(it.second)
+        if(intersect == it.first.toSet() || intersect == it.second.toSet()) 1 else 0
+    }.sum())
+}
